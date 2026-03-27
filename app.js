2d583b4111eef4d2a04bcfdcd5f4c0404343a0de
surajrana2 <surajpratapsinghrana2@gmail.com>
2026-03-28 02:12:28 +0530

    #conversion in js

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
-console.log("Hello World")
+// let score = ran
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+//"true" =. 1; false = 0
+// let isLoggedIn = "Rana_ji"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 =>false ; 
+//"" =>false
+//"hitesh" => true
+
+let SomeNumber = 33
+
+let stringNumber = String(SomeNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
 
 
 
