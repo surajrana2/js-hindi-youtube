0d90cf0db4c4e8f7a327d2a8afe1874243eccc12
surajrana2 <surajpratapsinghrana2@gmail.com>
2026-03-30 01:21:44 +0530

    2nd

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-// let score = ran
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
 
-// console.log(typeof score);
-// console.log(typeof (score));
+// console.log(2+2);
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+// let str1 = "hello"
+// let str2 = "rana"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2")
+
+// console.log( (3 + 4) * 5 % 3);//
 
 
-//"33" => 33
-//"33abc" => NaN
-//"true" =. 1; false = 0
-// let isLoggedIn = "Rana_ji"
 
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
 
-//1 => true; 0 =>false ; 
-//"" =>false
-//"hitesh" => true
+let num1, num2, num3
 
-let SomeNumber = 33
+num1 = num2 =num3  = 2 + 2
 
-let stringNumber = String(SomeNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
-
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
 
 
 
